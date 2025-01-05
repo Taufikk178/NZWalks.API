98f54195e949bffe12656f66320a7fee5c91b06e
Taufik khan <taufikk178@gmail.com>
2025-01-06 01:20:20 +0530

    Added CRUD Operation

---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,18 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInkm { get; set; }
+        public string? RegionImageUrl { get; set; }
+        public int MyProperty { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+        // Navigation Property
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+    }
+}
